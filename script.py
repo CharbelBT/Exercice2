a522ae344f4161fa6e6fa6dbbb8adb452feffdf7
Charbel Bou Tanios <c_boutanios@hetic.eu>
2024-01-16 11:58:23 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,14 @@
+import os
+import yaml
+
+
+with open('manual.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+env_var1 = config['env']['a']
+env_var2 = config['env']['b']
+
+os.environ['ENV_VAR1'] = env_var1
+os.environ['ENV_VAR2'] = env_var2
+
+print("La multiplication de a et b est : ", os.environ['ENV_VAR1']*os.environ['ENV_VAR2'])
